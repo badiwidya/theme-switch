c305a72b2f4fb8f2f4fb5b67ac09a38a391a5093
badiwidya <adiwidya.berton13@gmail.com>
2025-07-16 09:24:44 +0700

    Add configurationMap in themeSwitch function

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func main() {
 	configDir, err := os.UserConfigDir()
 	if err != nil {
-		fmt.Printf("Error: can't load config directory. Make sure your $HOME or $XDG_CONFIG_DIR is defined.\n%v\n", err)
+		fmt.Printf("Error: can't get config path. Make sure your $HOME or $XDG_CONFIG_HOME is defined.\n%v\n", err)
 		os.Exit(1)
 	}
 
@@ -26,4 +27,16 @@ func main() {
 		fmt.Printf("USAGE: theme-switch light|dark.")
 		os.Exit(1)
 	}
+}
+
+func switchTheme(theme, configDir string) error {
+	configurationMap := map[string]string{
+		"kitty-theme.conf":     filepath.Join(configDir, "kitty", "theme.conf"),
+		"waybar-colors.css":    filepath.Join(configDir, "waybar", "colors.css"),
+		"tofi-colors":          filepath.Join(configDir, "tofi", "colors"),
+		"hyprland-colors.conf": filepath.Join(configDir, "hypr", "colors.conf"),
+		"dunstrc":              filepath.Join(configDir, "dunst", "dunstrc"),
+	}
+
+	return nil
 }
